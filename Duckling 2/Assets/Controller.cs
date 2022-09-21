5c3225e7c036133c2f39df43f8700842322a19c4
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2022-09-21 12:27:25 +0200

    Added chase AI

--- Duckling 2/Assets/Controller.cs
+++ Duckling 2/Assets/Controller.cs
@@ -6,7 +6,7 @@ public class Controller : MonoBehaviour
 {
     private CharacterController characterController;
     public float speed = 5f;
-    public float rotationspeed = 75f;
+    public float rotationspeed = 150f;
     // Start is called before the first frame update
     private Vector3 Rotation;
 
@@ -18,9 +18,23 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float gravity = -0.01f;
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = 10f;
+        }
+                
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = 5f;
+        }
+        
+        
+
+
         this.Rotation = new Vector3(0, Input.GetAxis("Mouse X")*rotationspeed*Time.deltaTime, 0);
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime, -1,  Input.GetAxis("Vertical")*Time.deltaTime);
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal")*Time.deltaTime, gravity,  Input.GetAxis("Vertical")*Time.deltaTime);
     
 
         move = this.transform.TransformDirection(move);
