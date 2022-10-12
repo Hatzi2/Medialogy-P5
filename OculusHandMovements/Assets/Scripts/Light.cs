4ae85c082eb2e6b7317b950871c3ae13b812f442
Mads3132 <79976079+Mads3132@users.noreply.github.com>
2022-10-12 09:41:53 +0200

    LightSwitch script
    Added a grab interactor and script to lightswitch flipper. Needs tweaking, can't test without headset

---
+++ OculusHandMovements/Assets/Scripts/Light.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light : MonoBehaviour
+{
+    public GameObject PLight;
+    void Start()
+    {
+        
+    }
+    //Måske lav det sådan at der er en count der går up hver gang knappen bliver "grabbed" og resetter tilbage når if statement bliver kørt, 
+    //på den måde activeres scripted og overwriter den initial GameObjectSetActive i grab interact controlleren kun hver anden gang der bliver
+    //trykket? Altså så vil lyset tænde der me thinks, har ikk et headset så kan ikk lige teste noget - Mads 12-10-2022
+    void Update()
+    {
+        if(!PLight.activeSelf){
+            PLight.SetActive(true);
+        }
+    }
+}
