32b37e101c7aaea38d9199c694d63523c614d04f
kristian1402 <ksaren20@student.aau.dk>
2022-10-14 13:54:34 +0200

    fikset småting

--- OculusHandMovements/Assets/Scripts/Screw.cs
+++ OculusHandMovements/Assets/Scripts/Screw.cs
@@ -14,10 +14,10 @@ public class Screw : MonoBehaviour
     void Update()
     {
         while(duration < unscrewTime){
+            ScrewSound.Play();
             duration += Time.deltaTime;
             transform.position = transform.position + new Vector3(-1 * unscrewLength * Time.deltaTime, 0, 0);
             transform.Rotate(Vector3.right, 25.0f * Time.deltaTime, Space.World);
-            ScrewSound.Play();
             currentScrew.GetComponent<Rigidbody>().useGravity = true;
             Destroy(currentScrew, 1);
         }
