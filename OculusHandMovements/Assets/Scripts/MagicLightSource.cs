e6a7ff9df2c31d29c4f79c713b74ca29ab6742e4
kristian1402 <ksaren20@student.aau.dk>
2022-10-26 14:38:45 +0200

    blacklight

--- OculusHandMovements/Assets/Scripts/MagicLightSource.cs
+++ OculusHandMovements/Assets/Scripts/MagicLightSource.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class MagicLightSource : MonoBehaviour
 {
     public Material reveal;
-    public Light light;
+    public Light blacklight;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +15,8 @@ public class MagicLightSource : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        reveal.SetVector("_LightPosition", light.transform.position);
-        reveal.SetVector("_LightDirection", -light.transform.forward); //*Forward angle of our light in relation to the worldPos.
+        reveal.SetVector("_LightPosition", blacklight.transform.position);
+        reveal.SetVector("_LightDirection", -blacklight.transform.forward); //*Forward angle of our light in relation to the worldPos.
        // reveal.SetFloat("_LightAngle", light.spotAngle); //*Only works for spotlights
     }
 }
