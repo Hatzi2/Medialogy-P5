520ff3ef825a493d3b5b435f417d85c66eaf2a13
kristian1402 <ksaren20@student.aau.dk>
2022-10-26 12:47:24 +0200

    Leget med lyset

--- OculusHandMovements/Assets/Scripts/Emergencylight.cs
+++ OculusHandMovements/Assets/Scripts/Emergencylight.cs
@@ -8,20 +8,16 @@ public class Emergencylight : MonoBehaviour
     public GameObject Wire2;
     public Light thisLight;
     public AudioSource Emergency;
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if(!Wire1.activeSelf && !Wire2.activeSelf){
+            Emergency.loop = true;
             float Timer = Mathf.PingPong(Time.time*2, 2);
-            Emergency.Play();
             if (Timer > 1)
             {
                 thisLight.enabled = false;
+                Emergency.Play();
             }
             else
             {
