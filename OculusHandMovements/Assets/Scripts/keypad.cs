e2bb670e739e5188f2195714af7c81a0021d3b78
RimouZzz <jtraru19@student.aau.dk>
2022-11-14 12:28:16 +0100

    sound

--- OculusHandMovements/Assets/Scripts/keypad.cs
+++ OculusHandMovements/Assets/Scripts/keypad.cs
@@ -6,9 +6,13 @@ public class keypad : MonoBehaviour
 {
     public string outputNumber;
     public keypadScreen screen;
+    public AudioSource keyaudio;
+
 
     void OnCollisionEnter(Collision collision)
     {
         screen.writeToScreen(outputNumber);
+        GetComponent<AudioSource>().Play();
+
     }
 }
