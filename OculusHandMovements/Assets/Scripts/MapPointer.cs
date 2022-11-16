bb51dbe3d21bdf9157b65b0eff59ad71c9a5700b
kristian1402 <ksaren20@student.aau.dk>
2022-11-16 13:32:26 +0100

    :alien:

--- OculusHandMovements/Assets/Scripts/MapPointer.cs
+++ OculusHandMovements/Assets/Scripts/MapPointer.cs
@@ -1,30 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.Interaction.Toolkit.Inputs;
-using UnityEngine.XR;
-using System.Linq;
+
 public class MapPointer : MonoBehaviour
 {
-     private GameObject wayPoint;
-     private Vector3 wayPointPos;
-    private Vector3 headPosition;
-    private Quaternion headRotation;
-    private List<XRNodeState> nodeStates = new List<XRNodeState>();
-     //public GameObject MainCamera;
-     //List<InputDevice> HeadOrientation = new List<InputDevice>(); 
-     private float speed = 20.0f;
+     
+ private GameObject wayPoint;
+ private Vector3 wayPointPos;
+ private GameObject MainCamera;
+
+ private float speed = 20.0f;
  
  void Start ()
  {
       wayPoint = GameObject.Find("wayPoint");
-      //MainCamera = GameObject.Find("Main Camera");
-
-      
+      MainCamera = GameObject.Find("Main Camera");
  }
  
  void Update ()
  {   
+<<<<<<< HEAD
     InputTracking.GetNodeStates(nodeStates);
     var headState = nodeStates.FirstOrDefault(node => node.nodeType == XRNode.Head);
     
@@ -43,6 +38,11 @@ public class MapPointer : MonoBehaviour
     //transform.rotation = Quaternion.RotateTowards(transform.rotation, MainCamera.transform.rotation.y, speed*Time.deltaTime);
 
     transform.rotation = Quaternion.Euler(0,headRotation[1]*180,0);
+=======
+     wayPointPos = new Vector3(wayPoint.transform.position.x, transform.position.y, wayPoint.transform.position.z);
+     transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed * Time.deltaTime);
+     //transform.rotation = Quaternion.RotateTowards(transform.rotation, MainCamera.transform.rotation.y, speed*Time.deltaTime);
+     transform.rotation = Quaternion.Euler(0,MainCamera.transform.eulerAngles.y,0);
+>>>>>>> parent of 74e9183 (Added rotation that actually works)
  }
-
 }
