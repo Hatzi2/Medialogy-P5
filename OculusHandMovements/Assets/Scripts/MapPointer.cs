e3cf8186dba67cc3e209e3fb2d0921f5e5b4cb6a
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2022-11-22 15:03:17 +0100

    Removed rotation of minimap

--- OculusHandMovements/Assets/Scripts/MapPointer.cs
+++ OculusHandMovements/Assets/Scripts/MapPointer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit.Inputs;
 using UnityEngine.XR;
 using System.Linq;
+using Unity.Mathematics;
 public class MapPointer : MonoBehaviour
 {
      private GameObject wayPoint;
@@ -12,6 +13,7 @@ public class MapPointer : MonoBehaviour
     private Quaternion headRotation;
     private Vector3 headPositionPos;
     private List<XRNodeState> nodeStates = new List<XRNodeState>();
+    //private Vector3 reMappedValue;
      //public GameObject MainCamera;
      //List<InputDevice> HeadOrientation = new List<InputDevice>(); 
      private float speed = 20.0f;
@@ -30,12 +32,17 @@ public class MapPointer : MonoBehaviour
     var headState = nodeStates.FirstOrDefault(node => node.nodeType == XRNode.Head);
     
     headState.TryGetPosition(out headPosition);
-    headState.TryGetRotation(out headRotation);
-    
+    //headState.TryGetRotation(out headRotation);
+
+    //reMappedValue = new Vector3(0, headRotation[1], 0);
+    //transform.localEulerAngles = reMappedValue;
+    //Debug.Log(transform.localEulerAngles.y);
+    //headRotation.eulerAngles = reMappedValue;
 
     //UnityEngine.XR.InputDevices.GetDevicesAtXRNode(UnityEngine.XR.XRNode.Head, HeadOrientation);
     wayPointPos = new Vector3(headPosition[0], -10, headPosition[2]);
     transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed * Time.deltaTime);
+    
     //HeadOrientation = HeadOrientation[0].transform.localRotation.y; 
     //test = new Vector3(UnityEngine.XR.XRNode.Head.x, UnityEngine.XR.XRNode.Head.y, UnityEngine.XR.XRNode.Head.z);
 
@@ -43,7 +50,9 @@ public class MapPointer : MonoBehaviour
     //Debug.Log(UnityEngine.XR.InputTrackingState.Rotation);
     //transform.rotation = Quaternion.RotateTowards(transform.rotation, MainCamera.transform.rotation.y, speed*Time.deltaTime);
 
-    transform.rotation = Quaternion.Euler(0,headRotation[1]*180,0);
+    //transform.rotation = Quaternion.eulerAngles(0,headRotation[1],0);
+    //transform.rotation = headRotation;
+    //Debug.Log(headRotation);
     //headPositionPos = new Vector3(headPosition.x,0,headPosition.z);
     //transform.position = Vector3.MoveTowards(headPositionPos, headPositionPos, speed * Time.deltaTime);
  }
