54275c031a9513fe447c8580a0c7755abeffe864
kristian1402 <ksaren20@student.aau.dk>
2022-11-23 12:09:56 +0100

    Arrow done

--- OculusHandMovements/Assets/Scripts/WatchArrow.cs
+++ OculusHandMovements/Assets/Scripts/WatchArrow.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class WatchArrow : MonoBehaviour
 {
     public GameObject targetObject;
-    public float speed;
-
+    public float maxDistance;
+    private float speed = 100;
     private Vector3 GameObjectPosition;
+    Color colorStart = Color.cyan;
+    Color colorEnd = Color.red;
+    Renderer rend;
+    Renderer watchScreenMat;
 
-    // Start is called before the first frame update
     void Start()
     {
-        targetObject = GameObject.Find("Lightswitch");
+        var watchScreen = GameObject.Find("Screen");
+        watchScreenMat = watchScreen.GetComponent<Renderer>();
+        rend = GetComponent<Renderer>();
         GameObjectPosition = new Vector3(targetObject.transform.position.x, targetObject.transform.position.y, targetObject.transform.position.z);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float distance = Vector3.Distance (this.transform.position, targetObject.transform.position);
+        rend.material.color = Color.Lerp(colorStart, colorEnd, distance/maxDistance);
+        watchScreenMat.material.SetColor("_EmissionColor", Color.Lerp(colorStart, colorEnd, distance/maxDistance));
+        var watchLight = GameObject.Find("Watch Light").GetComponent<Light>();
+        watchLight.color = Color.Lerp(colorStart, colorEnd, distance/maxDistance);
         var lookPos = GameObjectPosition - transform.position;
         lookPos.y = 0;
         var rotation = Quaternion.LookRotation(lookPos);
